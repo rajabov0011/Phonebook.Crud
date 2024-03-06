90eccf9e4985e62d29f3b84276215ee3eee1770f
Asadbek Rajabov <154442057+rajabov0011@users.noreply.github.com>
2024-03-06 21:25:00 +0500

    Add Contact Finished

--- Program.cs
+++ Program.cs
@@ -1,59 +1,22 @@
-﻿internal class Program
+﻿using System;
+using Phonebook.Crud.Models;
+using Phonebook.Crud.Services.Contacts;
+
+namespace Phonebook.Crud
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-
-        void showMenu()
+        static void Main(string[] args)
         {
-            Console.WriteLine("\t-= Welcome to my project! =-");
-            Console.WriteLine("1. Create Phonebook");
-            Console.WriteLine("2. Update Phonebook");
-            Console.WriteLine("3. Delete Phonebook");
-            Console.WriteLine("4. Read All Phonebooks");
-            Console.WriteLine("5. Exit");
-        }
+            IContactService contactService = new ContactService();
 
-        bool retry = true;
-        while (retry)
-        {
-            Console.Clear();
-            showMenu();
-            Console.Write("Choose >>> ");
-            int myChoose = Convert.ToInt32(Console.ReadLine());
-            switch (myChoose)
+            Contact contact = new Contact
             {
-                case 1:
-                    Console.WriteLine("Create Phonebook, not implemented!");
-                    break;
-                case 2:
-                    Console.WriteLine("Update Phonebook, not implemented!");
-                    break;
-                case 3:
-                    Console.WriteLine("Delete Phonebook, not implemented!");
-                    break;
-                case 4:
-                    Console.WriteLine("Read All Phonbooks, not implemented!");
-                    break;
-                case 5:
-                    Console.WriteLine("Thanks, Bye!");
-                    retry = false;
-                    break;
-                default:
-                    Console.WriteLine("Wrong number selected!");
-                    break;
-            }
-            if (myChoose is not 5)
-            {
-                Console.Write("Continue? -> ");
-                string isContinue = Console.ReadLine();
-                if (isContinue == "yes")
-                    showMenu();
-                else
-                {
-                    Console.WriteLine("Thanks, Bye!");
-                    retry = false;
-                }
-            }   
+                Id = 1,
+                Name = "John Doe",
+                Phone = "123-456-7890"
+            };
+            contactService.addContact(contact);
         }
     }
 }
---
+++ Services/Contacts/ContactService.cs
@@ -0,0 +1,21 @@
+﻿using Phonebook.Crud.Brokers.Loggings;
+using Phonebook.Crud.Brokers.Storages;
+using Phonebook.Crud.Models;
+
+namespace Phonebook.Crud.Services.Contacts
+{
+    internal class ContactService : IContactService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public ContactService()
+        {
+            this.storageBroker = new FileStorageBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+
+        public Contact addContact(Contact contact) =>
+            this.storageBroker.AddContact(contact);
+    }
+}
