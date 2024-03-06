a66255f6a80dadfb9a83c8bb2a7ff43f08e7d030
Asadbek Rajabov <154442057+rajabov0011@users.noreply.github.com>
2024-03-06 15:15:40 +0500

    Create Contact

---
+++ Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Phonebook.Crud.Models
+{
+    internal class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
