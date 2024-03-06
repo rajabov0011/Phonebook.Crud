96e30f8ac29ea3c51fdc90344dd98d9ef774128d
Asadbek Rajabov <154442057+rajabov0011@users.noreply.github.com>
2024-03-06 22:28:31 +0500

    Codes Uodated

--- Brokers/Storages/IStorageBroker.cs
+++ Brokers/Storages/IStorageBroker.cs
@@ -5,5 +5,8 @@ namespace Phonebook.Crud.Brokers.Storages
     internal interface IStorageBroker
     {
         Contact AddContact(Contact contact);
+        Contact[] ReadAllContacts();
+        Contact UpdateContact(Contact contact);
+        bool DeleteContactById(int id);
     }
 }
