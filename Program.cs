dee1e8067fb2e2e7f733ed34c72e1d16f7d35a32
Asadbek Rajabov <154442057+rajabov0011@users.noreply.github.com>
2024-03-06 15:55:19 +0500

    Program.cs Updated

--- Program.cs
+++ Program.cs
@@ -2,6 +2,58 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        void showMenu()
+        {
+            Console.WriteLine("\t-= Welcome to my project! =-");
+            Console.WriteLine("1. Create Phonebook");
+            Console.WriteLine("2. Update Phonebook");
+            Console.WriteLine("3. Delete Phonebook");
+            Console.WriteLine("4. Read All Phonebooks");
+            Console.WriteLine("5. Exit");
+        }
+
+        bool retry = true;
+        while (retry)
+        {
+            Console.Clear();
+            showMenu();
+            Console.Write("Choose >>> ");
+            int myChoose = Convert.ToInt32(Console.ReadLine());
+            switch (myChoose)
+            {
+                case 1:
+                    Console.WriteLine("Create Phonebook, not implemented!");
+                    break;
+                case 2:
+                    Console.WriteLine("Update Phonebook, not implemented!");
+                    break;
+                case 3:
+                    Console.WriteLine("Delete Phonebook, not implemented!");
+                    break;
+                case 4:
+                    Console.WriteLine("Read All Phonbooks, not implemented!");
+                    break;
+                case 5:
+                    Console.WriteLine("Thanks, Bye!");
+                    retry = false;
+                    break;
+                default:
+                    Console.WriteLine("Wrong number selected!");
+                    break;
+            }
+            if (myChoose is not 5)
+            {
+                Console.Write("Continue? -> ");
+                string isContinue = Console.ReadLine();
+                if (isContinue == "yes")
+                    showMenu();
+                else
+                {
+                    Console.WriteLine("Thanks, Bye!");
+                    retry = false;
+                }
+            }   
+        }
     }
 }
